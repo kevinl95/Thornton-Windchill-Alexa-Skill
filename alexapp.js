786ed16aabbdd647a7e1d18de1ebcf81ee9b75d5
Kevin <kevin.loeffler@colorado.edu>
2018-11-25 14:52:29 -0700

    Removing Weather Underground code due to that API being deprecated, replacing with OpenWeatherMaps.

--- alexapp.js
+++ alexapp.js
@@ -2,7 +2,7 @@ exports.handler = function( event, context ) {
 
     var http = require( 'http' );
 
-    var url = 'Your Weather Underground link with API key goes here!';
+    var url = 'http://api.openweathermap.org/data/2.5/weather?zip=<YOURZIP>,us&units=imperial&APPID=<YOURAPPID>';
 
     http.get( url, function( response ) {
 
@@ -14,8 +14,16 @@ exports.handler = function( event, context ) {
 
             var json = JSON.parse( data );
 
-            var text = 'According to Weather Underground outside with wind chill the temperature feels like ';
-            text+=json.current_observation.feelslike_f+" degrees fahrenheit.";
+            var temp = json.main.temp;
+
+            var wind_speed = json.wind.speed;
+
+            var wind_chill = 35.74 + 0.6215*temp - 35.75*Math.pow(wind_speed, 0.16) + 0.4275*temp*Math.pow(wind_speed, 0.16);
+
+            var chill_rounded = Math.round( wind_chill * 10 ) / 10;
+
+            var text = 'Outside with wind chill the temperature feels like ';
+            text+=chill_rounded+" degrees fahrenheit.";
             output( text, context );
 
         } );
